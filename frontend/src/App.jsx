9e84230aa899f400bb94ea08c9286c08fd50d586
hughbuntine <hbuntine@student.unimelb.edu.au>
2025-02-16 15:43:01 +0200

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import Entry from './components/Entry';
+import ArchivedEntry from './components/ArchivedEntry';
 
 function App() {
+  const [newTask, setNewTask] = React.useState([]);
 
+  // FUNCTIONS
+  function addTask () {
+    console.log(newTask);
+  }
 
   return (
     <div>
-      <header>
+      <h1>
         TO-DO LIST
-      </header>
-      <Entry text={"get grogeries"}/> 
+      </h1>
+      <input type="text" placeholder="Add a new task" onChange={(e) => setNewTask(e.target.value)} />
+      <button onClick={() => addTask()}>Add Task</button>
+      <Entry text={"get groceries"}/> 
+      <h3>
+        COMPLETED
+      </h3>
+      <ArchivedEntry text={"walk the dog"}/>
     </div>
   );
 }
